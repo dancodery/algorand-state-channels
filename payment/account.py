1efcb3f4736eb4278b3ea043226e2d1a689449cc
Daniel Gockel <daniel@gockel.co>
2023-03-29 04:54:17 +0200

    variable private Key in account

--- payment/account.py
+++ payment/account.py
@@ -1,23 +1,32 @@
-from algosdk import account, mnemonic
-
+from algosdk import encoding, mnemonic
+from nacl.signing import SigningKey
+import base64
 
 class Account:
     """Represents a private key and address for an Algorand account"""
-    def __init__(self, privateKey: str) -> None:
-        self.sk = privateKey
-        self.addr = account.address_from_private_key(privateKey)
+    def __init__(self, *privateKey: str) -> None:
+        if len(privateKey) == 1:
+            self.signingKey = base64.b64decode(privateKey[0])[:32]
+            self.verifyKey = base64.b64decode(privateKey[0])[32:]
+            self.address = encoding.encode_address(self.verifyKey)
+            self.privateKey = privateKey[0]
+        else:     
+            self.signingKey = SigningKey.generate()
+            self.verifyKey = self.signingKey.verify_key
+            self.address = encoding.encode_address(self.verifyKey.encode())
+            self.privateKey = base64.b64encode(self.signingKey.encode() + self.verifyKey.encode()).decode()
 
     def getAddress(self) -> str:
-        return self.addr
+        return self.address
 
     def getPrivateKey(self) -> str:
-        return self.sk
+        return self.privateKey
     
     def getPublicKey(self) -> str:
-        return account.public_key_from_private_key(self.sk)
+        return self.verifyKey
     
     def getMnemonic(self) -> str:
-        return mnemonic.from_private_key(self.sk)
+        return mnemonic.from_private_key(self.privateKey)
     
     @classmethod
     def FromMnemonic(cls, m: str) -> "Account":
