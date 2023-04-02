5e3b159188d3923ed5ad53e27717e15f082154d2
Daniel Gockel <daniel@gockel.co>
2023-04-02 15:28:04 +0200

    funding temporary accounts

--- payment/testing/resources.py
+++ payment/testing/resources.py
@@ -1,10 +1,15 @@
 from typing import List
 
 from algosdk.v2client.algod import AlgodClient
+from algosdk.future import transaction
 from algosdk import account
 
 from ..account import Account
+from ..util import waitForTransaction
+from .setup import getGenesisAccounts
 
+
+FUNDING_AMOUNT = 10_000_000_000
 
 accountList: List[Account] = []
 
@@ -15,9 +20,32 @@ def getTemporaryAccount(client: AlgodClient) -> Account:
     # if account list is empty
     if len(accountList) == 0:
         # crete 16 new accounts
-        sks = [account.generate_account()[0] for i in range(16)]
-        accountList = [Account(sk) for sk in sks]
+        accountList = [Account() for i in range(16)]
 
-        # TODO load accounts with funds
+        genesisAccounts = getGenesisAccounts()
+        suggestedParams = client.suggested_params()
+
+        txns: List[transaction.Transaction] = []
+        # load accounts with funds
+        for i, a in enumerate(accountList):
+            fundingAccount = genesisAccounts[i % len(genesisAccounts)]
+            txns.append(
+                transaction.PaymentTxn(
+                    sender=fundingAccount.getAddress(),
+                    receiver=a.getAddress(),
+                    amt=FUNDING_AMOUNT,
+                    sp=suggestedParams,
+                )
+            )
+
+        txns = transaction.assign_group_id(txns)
+        signedTxns = [
+            txn.sign(genesisAccounts[i % len(genesisAccounts)].getPrivateKey())
+            for i, txn in enumerate(txns)
+        ]
+
+        client.send_transactions(signedTxns)
+
+        waitForTransaction(client, signedTxns[0].get_txid())
 
     return accountList.pop()
