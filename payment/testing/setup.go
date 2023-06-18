26ff4dc73f23b54eae9c82c68576b5fad3bb7877
Daniel Gockel <daniel@gockel.co>
2023-06-18 13:13:58 +0200

    adding testing setup functions

---
+++ payment/testing/setup.go
@@ -0,0 +1,61 @@
+package testing
+
+import (
+	"log"
+
+	"github.com/algorand/go-algorand-sdk/client/algod"
+	"github.com/algorand/go-algorand-sdk/v2/client/kmd"
+)
+
+const (
+	ALGOD_ADDRESS = "http://algorand-algod:4001"
+	ALGOD_TOKEN   = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+	KMD_ADDRESS = "http://algorand-algod:4002"
+	KMD_TOKEN   = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+	KMD_WALLET_NAME     = "unencrypted-default-wallet"
+	KMD_WALLET_PASSWORD = ""
+)
+
+func getAlgodClient() *algod.Client {
+	algodClient, err := algod.MakeClient(ALGOD_ADDRESS, ALGOD_TOKEN)
+	if err != nil {
+		log.Fatalf("failed to make algod client: %v\n", err)
+	}
+	return &algodClient
+}
+
+func getKmdClient() *algod.Client {
+	kmdClient, err := kmd.MakeClient(KMD_ADDRESS, KMD_TOKEN)
+	if err != nil {
+		log.Fatalf("Failed to create kmd client: %s", err)
+	}
+
+	return kmdClient
+}
+
+// func getGenesisAccounts() ([]*crypto.Account, error) {
+// 	kmdClient := getKmdClient()
+
+// 	response, err := kmdClient.ListWallets()
+
+// 	// get genesis accounts
+// 	algodClient := getAlgodClient()
+// 	genesisAccounts, err := algodClient.GetGenesisAccounts()
+// 	if err != nil {
+// 		log.Fatalf("failed to get genesis accounts: %v\n", err)
+// 		return nil, err
+// 	}
+
+// 	// convert genesis accounts to accounts
+// 	accounts := make([]*crypto.Account, len(genesisAccounts))
+// 	for i, genesisAccount := range genesisAccounts {
+// 		accounts[i] = &crypto.Account{
+// 			Address: genesisAccount.Address,
+// 			Amount:  genesisAccount.Amount,
+// 		}
+// 	}
+
+// 	return accounts, nil
+// }
