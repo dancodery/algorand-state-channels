550a024bd52fb0b5402ecbc283170c8609f610ae
Daniel Gockel <daniel@gockel.co>
2023-06-18 14:25:09 +0200

    add server instructions

--- asd.go
+++ asd.go
@@ -5,11 +5,15 @@ import (
 	"io"
 	"log"
 	"os"
+
+	"github.com/dancodery/algorand-state-channels/asrpc"
+	"google.golang.org/grpc"
 )
 
 func loadMain(out io.Writer) error {
 	log.SetOutput(out)
 
+	// load server config
 	loadedConfig, err := loadConfig()
 	if err != nil {
 		log.Fatalf("failed to load config: %v\n", err)
@@ -24,19 +28,18 @@ func loadMain(out io.Writer) error {
 	}
 	if err := server.start(); err != nil {
 		log.Fatalf("failed to start server: %v\n", err)
-		return err
 	}
 
-	//	start grpc server
-	// 	var opts []grpc.ServerOption
-	// 	grpcServer := grpc.NewServer()
+	// start grpc server
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	asrpc.RegisterASRPCServer(grpcServer, server.rpcServer)
+	fmt.Printf("Started grpc server on port %d\n", loadedConfig.PeerPort)
 
-	// 	ctx := context.Background()
-	// 	ctx, cancel := context.WithCancel(ctx)
-
-	// 	defer cancel()
-
-	// }
+	if err := grpcServer.Serve(server.listener); err != nil {
+		log.Fatalf("failed to serve: %v\n", err)
+		return err
+	}
 
 	return nil
 }
