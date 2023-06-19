454acc9efdc3f3cef23c69dab0b1de2bdf280365
Daniel Gockel <daniel@gockel.co>
2023-06-20 01:33:55 +0200

    extending open channel command

--- cmd/ascli/cmd_openchannel.go
+++ cmd/ascli/cmd_openchannel.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/dancodery/algorand-state-channels/asrpc"
 	"github.com/urfave/cli"
 )
 
@@ -16,19 +18,23 @@ var openChannelCommand = cli.Command{
 	ArgsUsage: "partner_ip funding_amount penalty_reserve dispute_window",
 	Flags: []cli.Flag{
 		cli.StringFlag{
-			Name:  "partner-ip",
+			Name:  "partner_ip",
 			Usage: "ip address of the partner node",
 		},
+		cli.StringFlag{
+			Name:  "partner_address",
+			Usage: "address of the partner node",
+		},
 		cli.Int64Flag{
-			Name:  "funding-amount",
+			Name:  "funding_amount",
 			Usage: "amount to fund the channel with",
 		},
 		cli.Int64Flag{
-			Name:  "penalty-reserve",
+			Name:  "penalty_reserve",
 			Usage: "amount to reserve for penalties",
 		},
 		cli.Int64Flag{
-			Name:  "dispute-window",
+			Name:  "dispute_window",
 			Usage: "number of blocks to wait for dispute resolution",
 		},
 	},
@@ -36,7 +42,46 @@ var openChannelCommand = cli.Command{
 }
 
 func openChannel(ctx *cli.Context) error {
-	fmt.Println("openChannel called")
+	if ctx.NArg() != 0 {
+		return cli.NewExitError("incorrect number of arguments", 1)
+	}
+	if ctx.String("partner_ip") == "" {
+		return cli.NewExitError("partner ip address is required", 1)
+	}
+	if ctx.String("partner_address") == "" {
+		return cli.NewExitError("partner address is required", 1)
+	}
+	if ctx.Int64("funding_amount") == 0 {
+		return cli.NewExitError("funding amount is required", 1)
+	}
+	if ctx.Int64("penalty_reserve") == 0 {
+		return cli.NewExitError("penalty reserve is required", 1)
+	}
+	if ctx.Int64("dispute_window") == 0 {
+		return cli.NewExitError("dispute window is required", 1)
+	}
+
+	ctxb := context.Background()
+	client := getClient(ctx)
+
+	nodeAddress := &asrpc.StateChannelNodeAddress{
+		Host:        ctx.String("partner_ip"),
+		AlgoAddress: ctx.String("partner_address"),
+	}
+
+	openChannelRequest := &asrpc.OpenChannelRequest{
+		PartnerNode:    nodeAddress,
+		FundingAmount:  ctx.Uint64("funding_amount"),
+		PenaltyReserve: ctx.Uint64("penalty_reserve"),
+		DisputeWindow:  ctx.Uint64("dispute_window"),
+	}
+
+	openChannelResponse, err := client.OpenChannel(ctxb, openChannelRequest)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(openChannelResponse)
 
 	return nil
 }
