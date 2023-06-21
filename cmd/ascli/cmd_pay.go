f1b44414f9f52ec5e4f7db713e4e51344c18e095
Daniel Gockel <daniel@gockel.co>
2023-06-21 14:51:46 +0200

    pay cli function

--- cmd/ascli/cmd_pay.go
+++ cmd/ascli/cmd_pay.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/dancodery/algorand-state-channels/asrpc"
 	"github.com/urfave/cli"
 )
 
@@ -16,6 +18,14 @@ var payCommand = cli.Command{
 	ArgsUsage: "amount",
 	Flags: []cli.Flag{
 		cli.StringFlag{
+			Name:  "partner_ip",
+			Usage: "ip address of the partner node",
+		},
+		cli.StringFlag{
+			Name:  "partner_address",
+			Usage: "address of the partner node",
+		},
+		cli.StringFlag{
 			Name:  "amount",
 			Usage: "amount to pay the channel partner",
 		},
@@ -24,7 +34,38 @@ var payCommand = cli.Command{
 }
 
 func pay(ctx *cli.Context) error {
-	fmt.Println("pay called")
+	if ctx.NArg() != 0 {
+		return cli.NewExitError("incorrect number of arguments", 1)
+	}
+	if ctx.String("partner_ip") == "" {
+		return cli.NewExitError("partner ip address is required", 1)
+	}
+	if ctx.String("partner_address") == "" {
+		return cli.NewExitError("partner address is required", 1)
+	}
+	if ctx.String("amount") == "" {
+		return cli.NewExitError("amount is required", 1)
+	}
+
+	nodeAddress := &asrpc.StateChannelNodeAddress{
+		Host:        ctx.String("partner_ip"),
+		AlgoAddress: ctx.String("partner_address"),
+	}
+
+	payRequest := &asrpc.PayRequest{
+		PartnerNode: nodeAddress,
+		Amount:      ctx.Uint64("amount"),
+	}
+
+	ctxb := context.Background()
+	client := getClient(ctx)
+
+	payResponse, err := client.Pay(ctxb, payRequest)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(payResponse)
 
 	return nil
 }
