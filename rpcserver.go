9828c16e28780ef8ff20cb088c1d317c6f241fcb
Daniel Gockel <daniel@gockel.co>
2023-06-21 13:44:49 +0200

    save onchain state and adding payment function

--- rpcserver.go
+++ rpcserver.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/dancodery/algorand-state-channels/asrpc"
 	"github.com/dancodery/algorand-state-channels/payment"
@@ -52,7 +53,7 @@ func (r *rpcServer) OpenChannel(ctx context.Context, in *asrpc.OpenChannelReques
 		in.PartnerNode.AlgoAddress,
 		in.PenaltyReserve,
 		in.DisputeWindow)
-	fmt.Printf("Created payment app with appID: %v\n", appID)
+	fmt.Printf("Created payment channel app with appID: %v\n", appID)
 
 	// 2. Fund payment app
 	payment.SetupPaymentApp(
@@ -61,8 +62,24 @@ func (r *rpcServer) OpenChannel(ctx context.Context, in *asrpc.OpenChannelReques
 		r.server.algo_account,
 		in.FundingAmount)
 
-	// 3. Notify partner node
-	notifyOpenChannel(in.PartnerNode.Host, appID)
+	// 3. send notification to partner node
+	sendRequest(in.PartnerNode.Host, P2PRequest{Command: "open_channel_request", Args: []string{strconv.FormatUint(appID, 10)}})
+
+	// 4. save payment channel on chain state
+	onchain_state := &paymentChannelOnChainState{
+		app_id:               appID,
+		alice_address:        r.server.algo_account.Address.String(),
+		bob_address:          in.PartnerNode.AlgoAddress,
+		alice_latest_balance: in.FundingAmount,
+		bob_latest_balance:   0,
+		total_deposit:        in.FundingAmount,
+		penalty_reserve:      in.PenaltyReserve,
+		dispute_window:       in.DisputeWindow,
+	}
+	r.server.payment_channels_onchain_states[in.PartnerNode.AlgoAddress] = *onchain_state
+
+	// print payment channel states
+	fmt.Printf("Payment channel states: %v\n", r.server.payment_channels_onchain_states)
 
 	timestamp_end := timestamppb.Now()
 
@@ -71,9 +88,50 @@ func (r *rpcServer) OpenChannel(ctx context.Context, in *asrpc.OpenChannelReques
 		TimestampEnd:   timestamp_end,
 	}
 
-	fmt.Println(in)
 	return &asrpc.OpenChannelResponse{
 		AppId:            appID,
+		RuntimeRecording: runtime_recording,
+	}, nil
+}
+
+func (r *rpcServer) Pay(ctx context.Context, in *asrpc.PayRequest) (*asrpc.PayResponse, error) {
+	timestamp_start := timestamppb.Now()
+
+	// 1. get on chain state
+	onchain_state := r.server.payment_channels_onchain_states[in.PartnerNode.AlgoAddress]
+
+	// 1. retrieve old balances
+	alice_balance := onchain_state.alice_latest_balance
+	bob_balance := onchain_state.bob_latest_balance
+
+	// 2. calculate new balances
+	new_alice_balance := alice_balance - in.Amount
+	new_bob_balance := bob_balance + in.Amount
+
+	//
+
+	// 3. sign new state
+	payment.SignState(
+		r.server.algod_client,
+		onchain_state.app_id,
+		r.server.algo_account,
+		new_alice_balance,
+		new_bob_balance,
+		4161)
+
+	// 3. send new state to partner node
+
+	// send pay request to partner node
+	sendRequest(in.PartnerNode.Host, P2PRequest{Command: "pay", Args: []string{strconv.FormatUint(in.Amount, 10)}})
+
+	timestamp_end := timestamppb.Now()
+
+	runtime_recording := &asrpc.RuntimeRecording{
+		TimestampStart: timestamp_start,
+		TimestampEnd:   timestamp_end,
+	}
+
+	return &asrpc.PayResponse{
 		RuntimeRecording: runtime_recording,
 	}, nil
 }
