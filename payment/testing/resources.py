cc4161475b2b6e0eed40aefa2fd56158035d0bab
Daniel Gockel <daniel@gockel.co>
2023-06-25 21:28:52 +0200

    support for manual seed phrase

--- payment/testing/resources.py
+++ payment/testing/resources.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from algosdk.v2client.algod import AlgodClient
 from algosdk import transaction
@@ -14,38 +14,60 @@ FUNDING_AMOUNT = 10_000_000_000
 accountList: List[Account] = []
 
 
-def getTemporaryAccount(client: AlgodClient) -> Account:
-    global accountList
+def getTemporaryAccount(client: AlgodClient, seed_phrase: Optional[str] = None) -> Account:
+    if seed_phrase:
+        account = Account.FromMnemonic(seed_phrase)
+    else:
+        account = Account()
 
-    # if account list is empty
-    if len(accountList) == 0:
-        # crete 16 new accounts
-        accountList = [Account() for i in range(16)]
+    genesisAccounts = getGenesisAccounts()
+    suggestedParams = client.suggested_params()
 
-        genesisAccounts = getGenesisAccounts()
-        suggestedParams = client.suggested_params()
+    txn = transaction.PaymentTxn(
+        sender=genesisAccounts[0].getAddress(),
+        receiver=account.getAddress(),
+        amt=FUNDING_AMOUNT,
+        sp=suggestedParams,
+    )
+    signedTxn = txn.sign(genesisAccounts[0].getPrivateKey())
 
-        txns: List[transaction.Transaction] = []
-        # load accounts with funds
-        for i, a in enumerate(accountList):
-            fundingAccount = genesisAccounts[i % len(genesisAccounts)]
-            txns.append(
-                transaction.PaymentTxn(
-                    sender=fundingAccount.getAddress(),
-                    receiver=a.getAddress(),
-                    amt=FUNDING_AMOUNT,
-                    sp=suggestedParams,
-                )
-            )
+    client.send_transaction(signedTxn)
 
-        txns = transaction.assign_group_id(txns)
-        signedTxns = [
-            txn.sign(genesisAccounts[i % len(genesisAccounts)].getPrivateKey())
-            for i, txn in enumerate(txns)
-        ]
+    waitForTransaction(client, signedTxn.get_txid())
 
-        client.send_transactions(signedTxns)
 
-        waitForTransaction(client, signedTxns[0].get_txid())
+    ##### OLD CODE #####
+    # global accountList
 
-    return accountList.pop()
+    # # if account list is empty
+    # if len(accountList) == 0:
+    #     # crete 16 new accounts
+    #     accountList = [Account() for i in range(16)]
+
+    #     genesisAccounts = getGenesisAccounts()
+    #     suggestedParams = client.suggested_params()
+
+    #     txns: List[transaction.Transaction] = []
+    #     # load accounts with funds
+    #     for i, a in enumerate(accountList):
+    #         fundingAccount = genesisAccounts[i % len(genesisAccounts)]
+    #         txns.append(
+    #             transaction.PaymentTxn(
+    #                 sender=fundingAccount.getAddress(),
+    #                 receiver=a.getAddress(),
+    #                 amt=FUNDING_AMOUNT,
+    #                 sp=suggestedParams,
+    #             )
+    #         )
+
+    #     txns = transaction.assign_group_id(txns)
+    #     signedTxns = [
+    #         txn.sign(genesisAccounts[i % len(genesisAccounts)].getPrivateKey())
+    #         for i, txn in enumerate(txns)
+    #     ]
+
+    #     client.send_transactions(signedTxns)
+
+    #     waitForTransaction(client, signedTxns[0].get_txid())
+
+    # return accountList.pop()
