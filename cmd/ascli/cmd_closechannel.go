24a082fe4bfd4118d44b8e26a4240705e3518036
Daniel Gockel <daniel@gockel.co>
2023-06-27 03:05:25 +0200

    add close channel command

--- cmd/ascli/cmd_closechannel.go
+++ cmd/ascli/cmd_closechannel.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/dancodery/algorand-state-channels/asrpc"
 	"github.com/urfave/cli"
 )
 
@@ -11,13 +13,38 @@ var closeChannelCommand = cli.Command{
 	Usage: "close an existing channel",
 	Description: `
 		Close an existing channel.
+		TODO: add more description here
 	`,
-	ArgsUsage: "no arguments",
-	Action:    closeChannel,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "partner_address",
+			Usage: "algo address of the partner node",
+		},
+	},
+	Action: closeChannel,
 }
 
 func closeChannel(ctx *cli.Context) error {
-	fmt.Println("closeChannel called")
+	if ctx.NArg() != 0 {
+		return cli.NewExitError("incorrect number of arguments", 1)
+	}
+	if ctx.String("partner_address") == "" {
+		return cli.NewExitError("partner algo address is required", 1)
+	}
+
+	close_channel_request := &asrpc.CloseChannelRequest{
+		AlgoAddress: ctx.String("partner_address"),
+	}
+
+	ctxb := context.Background()
+	client := getClient(ctx)
+
+	close_channel_response, err := client.CloseChannel(ctxb, close_channel_request)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(close_channel_response)
 
 	return nil
 }
