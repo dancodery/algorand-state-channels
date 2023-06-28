ae0c4d3468c83933adada0284f62df7f61d45cd0
Daniel Gockel <daniel@gockel.co>
2023-06-28 17:51:37 +0200

    change order

--- setup_testbed.sh
+++ setup_testbed.sh
@@ -4,25 +4,20 @@
 # 1. Read node arguments into an array
 args=("$@")
 
-echo "${args[@]}"
-
 # 2. Allocate nodes
+echo "Allocating nodes ${args[@]}..."
+pos allocations allocate ${args[@]}
+
+# 3. Configure nodes
 for ((i=0; i<${#args[@]}; i++)); do
-    # echo 
-    # echo "Freeing node ${args[i]}..."
-    # pos allocations free ${args[i]}
-
-    echo "Allocating node ${args[i]}..."
-    pos allocations allocate ${args[i]}
-
     echo "Configuring node ${args[i]}..."
     pos nodes image ${args[i]} debian-bullseye
 
-    echo "Reset node ${args[i]}..."
-    pos nodes reset ${args[i]}
+    # echo "Reset node ${args[i]}..."
+    # pos nodes reset ${args[i]}
 
-    echo "Launching commands on node ${args[i]}"
-    pos commands launch ${args[i]} "echo \$(hostname)"
+    # echo "Launching commands on node ${args[i]}"
+    # pos commands launch ${args[i]} "echo \$(hostname)"
 done
 
 
