2aedd7d68497e2f5a3c97fa9431174e2314dcfd5
Daniel Gockel <daniel@gockel.co>
2023-07-02 03:18:23 +0200

    raisedispute for watchtower

--- watchtower.go
+++ watchtower.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"strconv"
 	"time"
+
+	"github.com/dancodery/algorand-state-channels/payment"
 )
 
 func (s *server) UpdateWatchtowerState() {
@@ -18,6 +20,7 @@ func (s *server) UpdateWatchtowerState() {
 					log.Fatalf("Error reading smart contract from blockchain: %v\n", err)
 					return
 				}
+				// fmt.Printf("Smart contract info: %+v\n", blockchain_app_info)
 				// check if the channel is in the closing phase
 				timeout_bytes := GetValueOfGlobalState(blockchain_app_info.Params.GlobalState, "timeout")
 				if timeout_bytes == nil {
@@ -47,18 +50,6 @@ func (s *server) UpdateWatchtowerState() {
 						continue
 					}
 
-					// parse on chain variables
-					onchain_latest_timestamp_bytes := GetValueOfGlobalState(blockchain_app_info.Params.GlobalState, "latest_timestamp")
-					if onchain_latest_timestamp_bytes == nil {
-						log.Fatalf("Error: latest_timestamp not found in smart contract\n")
-						continue
-					}
-					onchain_latest_timestamp, err := strconv.ParseInt((string(onchain_latest_timestamp_bytes)), 10, 64)
-					if err != nil {
-						log.Fatalf("Error parsing latest_timestamp: %v\n", err)
-						continue
-					}
-
 					onchain_latest_alice_balance_bytes := GetValueOfGlobalState(blockchain_app_info.Params.GlobalState, "latest_alice_balance")
 					if onchain_latest_alice_balance_bytes == nil {
 						log.Fatalf("Error: latest_alice_balance not found in smart contract\n")
@@ -83,38 +74,34 @@ func (s *server) UpdateWatchtowerState() {
 
 					// check if the onchainstate is beneficial for me
 					var onchain_my_balance uint64
-					// var onchain_partner_balance uint64
 					var offchain_my_balance uint64
-					// var offchain_partner_balance uint64
 					if is_alice {
 						onchain_my_balance = onchain_latest_alice_balance
-						// onchain_partner_balance = onchain_latest_bob_balance
 						offchain_my_balance = latestOffChainState.alice_balance
-						// offchain_partner_balance = latestOffChainState.bob_balance
 					} else {
 						onchain_my_balance = onchain_latest_bob_balance
-						// onchain_partner_balance = onchain_latest_alice_balance
 						offchain_my_balance = latestOffChainState.bob_balance
-						// offchain_partner_balance = latestOffChainState.alice_balance
 					}
 
-					if onchain_my_balance > offchain_my_balance {
+					if onchain_my_balance >= offchain_my_balance {
 						// this case is beneficial for me
-						fmt.Printf("Latest balances are beneficial for me, I don't want to dispute\n")
+						fmt.Printf("Latest balances are beneficial for me, I don't want to dispute\n\n")
 						continue
 					}
 
 					// if the latest balances are not correct, we need to dispute
-					fmt.Printf("Latest balances are not correct, need to dispute\n")
-					fmt.Printf("On chain latest timestamp: %v\n", onchain_latest_timestamp)
-					fmt.Printf("On chain latest alice balance: %v\n", onchain_latest_alice_balance)
-					fmt.Printf("On chain latest bob balance: %v\n", onchain_latest_bob_balance)
-					fmt.Printf("Off chain latest timestamp: %v\n", latestOffChainState.timestamp)
-					fmt.Printf("Latest alice balance: %v\n", latestOffChainState.alice_balance)
-					fmt.Printf("Latest bob balance: %v\n", latestOffChainState.bob_balance)
+					fmt.Printf("Latest balances are not beneficial for me, I want to dispute\n\n")
 
-					// read the latest balances from the blockchain
-					fmt.Println("Timeout is ", timeout)
+					payment.RaiseDispute(
+						s.algod_client,
+						s.algo_account,
+						4161,
+						payment_channel_onchain_state.app_id,
+						latestOffChainState.alice_balance,
+						latestOffChainState.bob_balance,
+						uint64(latestOffChainState.timestamp),
+						latestOffChainState.alice_signature,
+						latestOffChainState.bob_signature)
 				}
 			}
 
