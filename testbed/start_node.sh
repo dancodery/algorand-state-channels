6c305405415d0c50ef29a944f2f2a530554a6e57
Daniel Gockel <daniel@gockel.co>
2023-07-04 19:07:50 +0200

    switch to ip

--- testbed/start_node.sh
+++ testbed/start_node.sh
@@ -3,7 +3,7 @@
 cd algorand-state-channels/
 docker build -t asc-my-node .
 docker run -d --name asc-my-node -p 28547:28547 \
-    -e ALGOD_ADDRESS="dogecoin.blockchain.net.in.tum.de:4001" \
-    -e KMD_ADDRESS="dogecoin.blockchain.net.in.tum.de:4002" \
-    -e INDEXER_ADDRESS="dogecoin.blockchain.net.in.tum.de:8980" \
+    -e ALGOD_ADDRESS="172.16.158.1:4001" \
+    -e KMD_ADDRESS="172.16.158.1:4002" \
+    -e INDEXER_ADDRESS="172.16.158.1:8980" \
         asc-my-node
