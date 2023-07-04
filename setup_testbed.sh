8cfabbeef301456d548154b6efffce36faf16ffa
Daniel Gockel <daniel@gockel.co>
2023-07-04 14:12:21 +0200

    uncomment alice and bob

--- setup_testbed.sh
+++ setup_testbed.sh
@@ -85,15 +85,15 @@ echo "Running sandbox on node ${sandbox_node}..."
 pos commands launch --infile testbed/run_sandbox.sh --queued --name run-sandbox ${sandbox_node}
 
 
-# # 10. Setup for alice and bob
-# alice_node=${args[1]}
-# bob_node=${args[2]}
+# 10. Setup for alice and bob
+alice_node=${args[1]}
+bob_node=${args[2]}
 
-# echo "Installing Docker on node ${alice_node}..."
-# pos commands launch --infile testbed/install_docker.sh --queued --name docker-setup ${alice_node}
+echo "Installing Docker on node ${alice_node}..."
+pos commands launch --infile testbed/install_docker.sh --queued --name docker-setup ${alice_node}
 
-# echo "Installing Docker on node ${bob_node}..."
-# pos commands launch --infile testbed/install_docker.sh --queued --name docker-setup ${bob_node}
+echo "Installing Docker on node ${bob_node}..."
+pos commands launch --infile testbed/install_docker.sh --queued --name docker-setup ${bob_node}
 
 
-# echo 
+echo 
