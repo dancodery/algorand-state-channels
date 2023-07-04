fc4ea9f2c5d246de2bc103935689b89a5e2887ac
Daniel Gockel <daniel@gockel.co>
2023-07-05 01:52:41 +0200

    add ram file

--- testbed/install_docker.sh
+++ testbed/install_docker.sh
@@ -12,3 +12,11 @@ echo \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+mkdir -p /etc/systemd/system/docker.service.d
+cat >/etc/systemd/system/docker.service.d/10-ramdisk.conf <<EOF
+[Service]
+Environment=DOCKER_RAMDISK=true
+EOF
+systemctl daemon-reload
+systemctl restart docker
