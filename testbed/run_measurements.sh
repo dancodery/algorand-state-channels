8baa638c6a010180d87cfcfda396906386736f75
Daniel Gockel <daniel@gockel.co>
2023-07-16 19:02:20 +0200

    try out initiate close

--- testbed/run_measurements.sh
+++ testbed/run_measurements.sh
@@ -92,16 +92,28 @@ if [ $(awk -v p=$dispute_probability 'BEGIN {print (rand() < p)}') -eq 1 ]; then
 	echo "Bob finalizing channel closing..."
 	run-in-node ${bob_node} "ascli finalizeclosechannel --partner_address=${alice_address}"
 else
-	# Bob closes the channel cooperatively
-	echo 
-	echo "Bob closing the channel cooperatively..."
-	# run-in-node ${alice_node} "ascli cooperativeclosechannel --partner_address=${bob_address}"
-	run-in-node ${bob_node} "ascli cooperativeclosechannel --partner_address=${alice_address}"
+	# # Bob closes the channel cooperatively
+	# echo 
+	# echo "Bob closing the channel cooperatively..."
+	# # run-in-node ${alice_node} "ascli cooperativeclosechannel --partner_address=${bob_address}"
+	# run-in-node ${bob_node} "ascli cooperativeclosechannel --partner_address=${alice_address}"
+
+	# Initiate closing the channel
+	echo
+	echo "Alice initiating channel closing..."
+	run-in-node ${alice_node} "ascli initiateclosechannel --partner_address=${bob_address}"
+
+	# sleep for dispute_window * block_time
+	echo
+	echo "Waiting for dispute window to expire: ${dispute_window} * 4 seconds..."
+	sleep $(echo "${dispute_window} * 4" | bc)
+
+	# Finalize closing the channel
+	echo
+	echo "Alice finalizing channel closing..."
+	run-in-node ${alice_node} "ascli finalizeclosechannel --partner_address=${bob_address}"
 fi
-# # Initiate closing the channel
-# echo
-# echo "Alice initiating channel closing..."
-# run-in-node ${alice_node} "ascli initiateclosechannel --partner_address=${bob_address}"
+
 
 # Get Alice and Bob's final balances
 alice_final_balance=$(run-in-node ${alice_node} "ascli getinfo | jq -r .algo_balance") # save Alice's balance as raw string
