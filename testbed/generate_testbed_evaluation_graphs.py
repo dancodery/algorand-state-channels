502f27ca233247b8d7061eb7d6e8d44d02a0a3fc
Daniel Gockel <daniel@gockel.co>
2023-07-20 18:23:18 +0200

    plot execution time

--- testbed/generate_testbed_evaluation_graphs.py
+++ testbed/generate_testbed_evaluation_graphs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -22,18 +21,28 @@ df['payment_amount'] = df.index.astype(int)
 # sort the dataframe by payment amount
 df = df.sort_values('payment_amount')
 
-# plot
+# plot transaction fees
 plt.figure(figsize=(10, 6))
-plt.plot(df['payment_amount'], df['transaction_fees'], marker='o', label='Transaction Fees la')
+plt.plot(df['payment_amount'], df['transaction_fees'], marker='o')
 
 plt.xlabel('Amount of Payments')
 plt.ylabel('Transaction Fees Savings')
-plt.legend()
+plt.legend(title=f"Dispute Window: {evaluation_results['dispute_window']}, Dispute Probability: {evaluation_results['dispute_probability']}")
 
 plt.xticks(range(1, 21))
 
 plt.savefig('results/transaction_fees_savings_graph.pdf')
 plt.show()
 
+# plot execution time
+plt.figure(figsize=(10, 6))
+plt.plot(df['payment_amount'], df['execution_time'], marker='o')
 
+plt.xlabel('Amount of Payments')
+plt.ylabel('Execution Time (seconds)')
+plt.legend(title=f"Dispute Window: {evaluation_results['dispute_window']}, Dispute Probability: {evaluation_results['dispute_probability']}")
 
+plt.xticks(range(1, 21))
+
+plt.savefig('results/transaction_time_savings_graph.pdf')
+plt.show()
