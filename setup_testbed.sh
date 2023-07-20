81c4589266f200e305a264e79c3c3719fa7dad4a
Daniel Gockel <daniel@gockel.co>
2023-07-20 11:39:24 +0200

    eno4

--- setup_testbed.sh
+++ setup_testbed.sh
@@ -207,7 +207,7 @@ echo
 
 # 13. Sync nodes
 echo "Syncing nodes..."
-interface="eno5"
+interface="eno4"
 pos commands launch --infile testbed/setup_sync_node.sh --name setup-sync-node ${alice_node}
 pos commands launch --infile testbed/setup_sync_node.sh --name setup-sync-node ${bob_node}
 
