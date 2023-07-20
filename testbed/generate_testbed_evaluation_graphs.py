4991f3ca67ee2d7264276b3fbe86cb088e99e90a
Daniel Gockel <daniel@gockel.co>
2023-07-21 01:25:06 +0200

    visualize savings

--- testbed/generate_testbed_evaluation_graphs.py
+++ testbed/generate_testbed_evaluation_graphs.py
@@ -16,28 +16,30 @@ df = pd.DataFrame(payment_data).T
 # convert to numeric
 df["transaction_fees"] = pd.to_numeric(df['transaction_fees'])
 df["execution_time"] = pd.to_numeric(df['execution_time'])
-
-# add numerical column for payment amount
-df['payment_amount'] = df.index.astype(int)
+df['payment_amount'] = df.index.astype(int)                                                     # add column with payment amount
+df['fee_savings'] = df['payment_amount'] * ON_CHAIN_TRANSACTION_FEE - df['transaction_fees']    # add column savings
 
 # sort the dataframe by payment amount
 df = df.sort_values('payment_amount')
 
-# plot transaction fees
+
+########## 1. plot transaction fees
 plt.figure(figsize=(10, 6))
-plt.plot(df['payment_amount'], df['transaction_fees'], marker='o')
+plt.plot(df['payment_amount'], df['fee_savings'], marker='o')
 
 plt.xlabel('Amount of Payments')
-plt.ylabel('Transaction Fees Savings (microAlgo)')
+plt.ylabel('Transaction Fee Savings (microAlgo)')
 plt.legend(title=f"Dispute Window: {evaluation_results['dispute_window']}, Dispute Probability: {evaluation_results['dispute_probability']}")
 
 plt.xticks(range(1, 21))
+plt.axhline(0, color='gray', linestyle='--')
 plt.grid(True)
 
 plt.savefig('results/transaction_fees_savings_graph.pdf')
 plt.show()
 
-# plot execution time
+
+########## 2. plot execution time
 plt.figure(figsize=(10, 6))
 plt.plot(df['payment_amount'], df['execution_time'], marker='o')
 
