7a92c15700d61538486d183c0994476cff4e6bf7
Daniel Gockel <daniel@gockel.co>
2023-07-20 21:13:45 +0200

    add constants

--- testbed/generate_testbed_evaluation_graphs.py
+++ testbed/generate_testbed_evaluation_graphs.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
+ON_CHAIN_TRANSACTION_FEE = 1000
+ON_CHAIN_TRANSACTION_DELAY = 3.3
 
 with open("results/experiment1_adjusted.json", "r") as evaluation_results_file:
     evaluation_results = json.load(evaluation_results_file)
@@ -26,7 +28,7 @@ plt.figure(figsize=(10, 6))
 plt.plot(df['payment_amount'], df['transaction_fees'], marker='o')
 
 plt.xlabel('Amount of Payments')
-plt.ylabel('Transaction Fees Savings')
+plt.ylabel('Transaction Fees Savings (microAlgo)')
 plt.legend(title=f"Dispute Window: {evaluation_results['dispute_window']}, Dispute Probability: {evaluation_results['dispute_probability']}")
 
 plt.xticks(range(1, 21))
@@ -39,10 +41,14 @@ plt.figure(figsize=(10, 6))
 plt.plot(df['payment_amount'], df['execution_time'], marker='o')
 
 plt.xlabel('Amount of Payments')
-plt.ylabel('Execution Time (seconds)')
+plt.ylabel('Execution Time Savings (seconds)')
 plt.legend(title=f"Dispute Window: {evaluation_results['dispute_window']}, Dispute Probability: {evaluation_results['dispute_probability']}")
 
 plt.xticks(range(1, 21))
 
 plt.savefig('results/transaction_time_savings_graph.pdf')
 plt.show()
+
+
+# plot transaction fees for 10000 to 200000 payments
+# plt.figure(figsize=(10, 6))
