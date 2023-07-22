a3b7f1e2a5a316f016ca99fcd284299968c817c3
Daniel Gockel <daniel@gockel.co>
2023-07-22 22:19:32 +0200

    devmode false

--- testbed/run_sandbox.sh
+++ testbed/run_sandbox.sh
@@ -3,4 +3,6 @@
 git clone https://github.com/algorand/sandbox.git
 cd sandbox/
 git checkout eae95b9
+file_path="images/algod/DevModeNetwork.json"
+sed -i 's/"DevMode": true/"DevMode": false/' "$file_path"
 ./sandbox up dev -v
