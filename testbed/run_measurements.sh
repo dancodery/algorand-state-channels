3e87ed4751aa9ddc1cbf88f7d63a9ba67cb5dbd4
Daniel Gockel <daniel@gockel.co>
2023-07-22 11:36:13 +0200

    dec

--- testbed/run_measurements.sh
+++ testbed/run_measurements.sh
@@ -45,7 +45,7 @@ wait-for-node ${bob_node} "ascli getinfo"
 
 payments_record="{"
 
-for ((how_many_payments=21; how_many_payments<=40; how_many_payments++)); do
+for ((how_many_payments=1; how_many_payments<=20; how_many_payments++)); do
 	if [ $how_many_payments -ge 21 ]; then
 		how_many_payments_final=$(( (how_many_payments - 20) * 100))
 	else 
@@ -60,7 +60,7 @@ for ((how_many_payments=21; how_many_payments<=40; how_many_payments++)); do
 	echo "Resetting Alice and Bob's nodes..."
 	run-in-node ${alice_node} "ascli reset"
 	run-in-node ${bob_node} "ascli reset"
-	
+
 	# Resetting Sandbox
 	echo "Resetting Sandbox..."
 	pos commands launch --name sandbox-reset ${sandbox_node} -- ./sandbox/sandbox reset -v
